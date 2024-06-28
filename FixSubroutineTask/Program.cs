b79f0aac6fb122dc6713f1fc8030dba1179b64be
Al Stone <astone@heckgrammar.co.uk>
2024-06-28 12:01:36 +0100

    Add project files.

---
+++ FixSubroutineTask/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace FixSubroutineTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 8;i >= 0; i++) {
+                Console.WriteLine(diffCurrencies(i));            
+            }
+        }
+        static string diffCurrencies(int x)
+        {
+            string[] currencies = { "baht", "dollar", "euro", "koruna", "lira", "rand", "rupee", "yen" };
+            return currencies[1];
+
+        }
+    }
+}
